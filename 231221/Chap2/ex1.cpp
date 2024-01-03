004e908a3aadae6da3e6cea74a28f32191faef25
rudwns04 <154478954+rudwns04@users.noreply.github.com>
2024-01-04 08:59:19 +0900

    Update and rename 231221/ex1.cpp to 231221/Chap2/ex1.cpp

--- 231221/Chap2/ex1.cpp
+++ 231221/Chap2/ex1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
 	for (int i = 1; i <= 100; i++) {
 		cout << i;
-		if (i % 10 == 0) // 10ÀÇ ¹è¼öÀÎ °æ¿ì
+		if (i % 10 == 0) // 10Ã€Ã‡ Â¹Ã¨Â¼Ã¶Ã€ÃŽ Â°Ã¦Â¿Ã¬
 			cout << endl;
 		else
 			cout << '\t';
