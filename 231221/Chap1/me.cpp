ccf819ada2b9244194aae3570415c7df2da407cc
rudwns04 <154478954+rudwns04@users.noreply.github.com>
2024-01-04 09:02:30 +0900

    Add files via upload

---
+++ 231221/Chap1/me.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+	std::cout << " 조선해양시스템공학과 \n 29세 \n 컴퓨터개발자 \n";
+	return 0;
+}
---
+++ 231221/Chap1/name.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+	std::cout << "My name is KyeungJun";
+	return 0;
+}
