73dda0bd77768881e5b169bb0b7f32341274dac4
rudwns04 <154478954+rudwns04@users.noreply.github.com>
2024-01-04 09:22:57 +0900

    Add files via upload

---
+++ 231228/Chap8/8-6.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Base {
+	int a;
+protected:
+	void setA(int a) { this->a = a; }
+public:
+	void showA() { cout << a; }
+};
+
+class Derived : private Base {
+	int b;
+protected:
+	void setB(int b) { this->b = b; }
+public:
+	void showB() {
+		setA(5); 		// ¬Ú¬é
+		showA(); 		// ¬Ú¬ê
+		cout << b;
+	}
+};
+
+class GrandDerived : private Derived {
+	int c;
+protected:
+	void setAB(int x) {
+		setA(x); 		// ¬Ú¬ë
+		showA(); 		// ¬Ú¬ì 
+		setB(x); 		// ¬Ú¬í
+	}
+};
